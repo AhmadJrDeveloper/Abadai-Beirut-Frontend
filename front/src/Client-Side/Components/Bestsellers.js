57bc34b216c2d4bf31198e4e407ae35acc9cb414
Malak <malakhmm22@gmail.com>
2023-10-30 20:38:37 +0200

    created carts component

--- front/src/Client-Side/Components/Bestsellers.js
+++ front/src/Client-Side/Components/Bestsellers.js
@@ -1,6 +1,5 @@
 import '../Styles/bestsellers.css'
-import Button from './MainButton'
-
+import Cart from './Carts'
 const Recommended = ()=>{
 
 
@@ -8,14 +7,14 @@ const Recommended = ()=>{
         <div className="Home-recommended">
             <p className="Home-Cart-Title">Our BestSellers</p>
             <div className="Home-All-Carts">
-            <div className="Home-Cart">
-                <img src="https://www.w3schools.com/html/img_chania.jpg"/>
-                <p className="Home-Product-Name">Shawarma</p>
-                <p className="Home-Product-Description">
-                    very delicious product
-                </p>
-                <Button/>
-            </div>
+                <Cart/>
+                <Cart/>
+                <Cart/>
+                <Cart/>
+                <Cart/>
+                <Cart/>
+                <Cart/>
+           
             </div>
 
         </div>
