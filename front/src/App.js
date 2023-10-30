9ec2529b4f27f165bfb4742a6d9fb63e09e5ecd0
ahmadjrdeveloper <ahmadsaad200235@gmail.com>
2023-10-30 16:56:32 +0200

    button component done

--- front/src/App.js
+++ front/src/App.js
@@ -1,10 +1,10 @@
-
+import Button from './Client-Side/Components/MainButton'
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Button />
     </div>
   );
 }
---
+++ front/src/Client-Side/Components/MainButton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../Styles/MainButton.css'
+
+export default function MainButton() {
+  return (
+    <div>
+        <button className='MainButton'>read more</button>
+    </div>
+  )
+}
+
+ 
