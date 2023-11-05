a7b20813162b5d29093bb46a42ffdb162d706fcd
Malak <malakhmm22@gmail.com>
2023-11-05 17:19:34 +0200

    Edit Category form page done

--- front/src/App.js
+++ front/src/App.js
@@ -4,14 +4,16 @@ import Message from '../src/Admin-Side/Components/Message'
 import Inbox from '../src/Admin-Side/Pages/Inbox'
 import SideBar from '../src/Admin-Side/Components/SideBar'
 import AddCat from './Admin-Side/Pages/AddCat.js';
-
+import EditCat from './Admin-Side/Pages/EditCat';
 function App() {
   return (
     <div className="App">
 
       <SideBar/>
       
-     < AddCat/>
+    
+
+     <EditCat/>
 
     </div>
   );
